4f555bdf44a18be2cbb78916e0952e4d73a9d764
Estudiante <Estudiante@LAB1004-PC27>
2017-01-02 17:50:03 -0600

    Sets Gets Cita

--- src/main/java/una/cr/design/model/Cita.java
+++ src/main/java/una/cr/design/model/Cita.java
@@ -5,17 +5,48 @@ public class Cita {
     Paciente paciente;
     Consultorio consultorio;
     String fecha;
+    boolean confirmar;
 
     public Cita() {
     }
 
-    public Cita(Paciente paciente, Consultorio consultorio, String fecha) {
+    public Cita(Paciente paciente, Consultorio consultorio, String fecha, boolean confirmar) {
         this.paciente = paciente;
         this.consultorio = consultorio;
         this.fecha = fecha;
+        this.confirmar = confirmar;
     }
 
-    public void horaConsulta() {
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Consultorio getConsultorio() {
+        return consultorio;
+    }
+
+    public void setConsultorio(Consultorio consultorio) {
+        this.consultorio = consultorio;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public boolean isConfirmar() {
+        return confirmar;
+    }
+
+    public void setConfirmar(boolean confirmar) {
+        this.confirmar = confirmar;
     }
 
     @Override
