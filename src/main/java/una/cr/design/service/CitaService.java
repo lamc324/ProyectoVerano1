2cf923859790c10159d301f62e290084596e16ff
luism <luism@DESKTOP-0QB9JPF>
2017-01-12 12:36:13 -0600

    - Licencias

--- src/main/java/una/cr/design/service/CitaService.java
+++ src/main/java/una/cr/design/service/CitaService.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2016 Grupo Tango
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Universidad Nacional de Costa Rica, Estudiantes: Luis Alonso Morgan Campos,
+ * John Herrera Jimenez, Kevin Venegas, Caleb Villalta, Josue David Matamorros.
+ */
 package una.cr.design.service;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
@@ -17,7 +36,15 @@ import una.cr.design.model.Cita;
 
 public class CitaService {
     
-      public Object[][] loadCitaObjWrapper() throws JsonGenerationException,
+    /**
+     * Carga el .JSON y envia un wrapper de los objetos
+     *
+     * @return 
+     * @throws JsonGenerationException
+     * @throws JsonMappingException
+     * @throws IOException
+     */
+    public Object[][] loadCitaObjWrapper() throws JsonGenerationException,
             JsonMappingException, IOException {
         Cita[] cita = cargarCitasDeArchivo();
         Object[][] data = null;
@@ -49,6 +76,7 @@ private Cita[] cargarCitasDeArchivo() throws JsonGenerationException,
 
         return citas;
     }
+
     private String checkIfNull(Object obj) {
         String text;
         if (obj == null) {
